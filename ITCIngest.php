6352bc45492e0e2b45d87b9b6aee20f3bb1b9182
Mark Beech <mbeech@mark-beech.co.uk>
2015-12-08 17:47:37 -0500

    Applied fixes from StyleCI

--- ITCIngest.php
+++ ITCIngest.php
@@ -13,14 +13,12 @@ class ITCIngest
         $this->username = $username;
         $this->password = $password;
         $this->vndnumber = $vndnumber;
-
     }
 
     public function getData($date)
     {
         $this->filename = "{$date}-{$this->vndnumber}";
-        if($this->sendRequest())
-        {
+        if ($this->sendRequest()) {
             $this->processCsv();
         }
     }
@@ -28,13 +26,13 @@ class ITCIngest
     public function buildParams()
     {
         return http_build_query([
-            'USERNAME' => $this->username,
-            'PASSWORD' => $this->password,
-            'VNDNUMBER' => $this->vndnumber,
-            'TYPEOFREPORT' =>'Sales',
-            'DATETYPE' => 'Daily',
-            'REPORTTYPE' => 'Summary',
-            'REPORTDATE' => $date,
+            'USERNAME'     => $this->username,
+            'PASSWORD'     => $this->password,
+            'VNDNUMBER'    => $this->vndnumber,
+            'TYPEOFREPORT' => 'Sales',
+            'DATETYPE'     => 'Daily',
+            'REPORTTYPE'   => 'Summary',
+            'REPORTDATE'   => $date,
         ]);
     }
 
@@ -49,7 +47,7 @@ class ITCIngest
         curl_setopt($ch, CURLOPT_POSTFIELDS, $this->buildParams());
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FILE, $fp);
-        
+
         //execute post
         return curl_exec($ch);
     }
@@ -58,27 +56,27 @@ class ITCIngest
     {
         if (filesize("$this->filename.gz")) {
             if (function_exists('gzdecode')) {
-                file_put_contents($this->filename, gzdecode(file_get_contents("$this->filename.gz")) );
+                file_put_contents($this->filename, gzdecode(file_get_contents("$this->filename.gz")));
             } else {
                 exec("gunzip $this->filename.gz");
             }
 
-            if (($handle = fopen("$this->filename", "r")) !== FALSE) {
+            if (($handle = fopen("$this->filename", 'r')) !== false) {
                 // get first line to use as array keys
                 $columns = fgetcsv($handle, 0, "\t");
 
-                while (($data = fgetcsv($handle, 0, "\t")) !== FALSE) {
+                while (($data = fgetcsv($handle, 0, "\t")) !== false) {
                     var_dump(array_combine($columns, $data));
                 }
 
                 fclose($handle);
-            }  else {
-                echo "Could not open $this->filename for reading" . PHP_EOL;
-            }   
-            
+            } else {
+                echo "Could not open $this->filename for reading".PHP_EOL;
+            }
+
             unlink("$this->filename");
         } else {
-            echo 'File is of size 0' . PHP_EOL;
+            echo 'File is of size 0'.PHP_EOL;
             unlink("$this->filename.gz");
         }
     }
